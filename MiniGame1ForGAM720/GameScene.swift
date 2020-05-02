69a835b6d5164cb559969dfd359097e952b5a594
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-05-02 20:06:17 +0100

    One error needs fixing. Fixed errors and spelling mistakes.

--- MiniGame1ForGAM720/GameScene.swift
+++ MiniGame1ForGAM720/GameScene.swift
@@ -5,85 +5,112 @@
 //  Created by Rachel Saunders on 30/04/2020.
 //  Copyright © 2020 Rachel Saunders. All rights reserved.
 //
+// THIS APP IS SOLELY FOR THE PURPOSE OF ASSIGNMENT 2 FOR GAM720 MODULE AS PART OF THE MASTERS DEGREE FOR CREATIVE APP DEVELOPMENT. ALL PROGRAMMING, NOTES AND ARTWORK BY RACHEL SAUNDERS. THIS IS A TEST TO SEE HOW MINIGAMES WORK. COPYRIGHT RACHEL SAUNDERS.
+//
+//
 
 import SpriteKit
-import GameplayKit
+
+// Numbers don't add to equal another e.g. 1 + 2 = 3 theres no 3 aka no colliding values
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case bird = 8
+    case finish = 16
+}
+
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        loadLevel()
+        
+        }
+    
+    
+    func loadLevel() {
+        
+        // Load the level, if it fails do nothing aka crash
+        // level is being loaded by letter arrangement in the file level1.txt
+        
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt")
+            else {
+                fatalError("couldn't find level file")
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let levelString = try? String(contentsOf: levelURL)
+            else {
+                fatalError("couldn't find level file")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        // defining the lines
+        // \n is a line break
+        let lines = levelString.components(separatedBy: "\n")
+        
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+               
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    
+                    let node = SKSpriteNode(imageNamed: "block")
+                    node.position = position
+                    node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+                    node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+                    node.physicsBody?.isDynamic = false
+                    
+                } else if letter == "v" {
+                    
+                    let node = SKSpriteNode(imageNamed: "bird")
+                    node.name = "bird"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+                    node.physicsBody?.categoryBitMask = CollisionTypes.bird.rawValue
+                    // When it touches the player
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    
+                    addChild(node)
+                    
+                } else if letter == "s" {
+                    
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.name = "star"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    
+                    addChild(node)
+                    
+                } else if letter == "f" {
+                    
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.name = "finish"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    
+                    addChild(node)
+                    
+                } else {
+                    fatalError("Couldn't read letters from file")
+                }
+            }
+        }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+        
+        
 }
